c483bc470731ee2eb13039ddc76f69fcd48f1540
CoryMci <corymciver@live.ca>
2022-09-04 15:46:16 -0700

    Add css flex to grid

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let gridsize = 64;
+let gridsize = 8;
+let boxcount = gridsize ** 2;
+let boxsize = 512 / gridsize;
 let grid = document.querySelector(".grid");
 
-function populateGrid (gridsize) {
-    for (i = 0; i < gridsize; i++) {
+function populateGrid (boxcount) {
+    for (i = 0; i < boxcount; i++) {
         let box = document.createElement("div")
         box.classList.add("box")
         box.id = i
+        box.style.width = `${boxsize}px`
+        box.style.height= `${boxsize}px`
         grid.appendChild(box)
-    }
+    }//populates grid with indexed boxes.
 
 }
 
-populateGrid(gridsize)
+populateGrid(boxcount)
