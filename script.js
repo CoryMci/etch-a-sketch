add0ffc11562da9b0448f463c06088ca64d25452
CoryMci <corymciver@live.ca>
2022-09-04 16:25:52 -0700

    Add listen and draw functions. First working prototype

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ let boxcount = gridsize ** 2;
 let boxsize = 512 / gridsize;
 let grid = document.querySelector(".grid");
 
-function populateGrid (boxcount) {
+function populateGrid(boxcount) {
     for (i = 0; i < boxcount; i++) {
-        let box = document.createElement("div")
-        box.classList.add("box")
-        box.id = i
-        box.style.width = `${boxsize}px`
-        box.style.height= `${boxsize}px`
-        grid.appendChild(box)
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.id = "box" + i;
+        box.style.width = `${boxsize}px`;
+        box.style.height= `${boxsize}px`;
+        grid.appendChild(box);
     }//populates grid with indexed boxes.
 
 }
 
-populateGrid(boxcount)
+function draw(targetid) {
+    let target = document.querySelector(`#${targetid}`);
+    target.style.backgroundColor = 'black';
+}
+
+
+function listen() {
+    let boxes = document.querySelectorAll(".box")
+    boxes.forEach(box => { 
+        box.addEventListener('mouseover', (e)=>{ //adds event listener to each box
+            console.log(e.target.id);
+            draw(e.target.id);
+        })
+    })
+}
+
+
+populateGrid(boxcount);
+listen();
